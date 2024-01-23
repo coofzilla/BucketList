32d2a24ece7eb305269a31394e1541fb8525c64c
coofzilla <jeric614@yahoo.com>
2024-01-23 16:37:52 +0900

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jeric Hernandez on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
