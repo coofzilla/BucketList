4ca0aba868f69d8b7ad7188189835ba4e51ebc54
coofzilla <jeric614@yahoo.com>
2024-01-24 13:41:08 +0900

    feature: basic map functionality

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,41 +5,24 @@
 //  Created by Jeric Hernandez on 1/23/24.
 //
 
+import MapKit
 import SwiftUI
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed")
-    }
-}
-
 struct ContentView: View {
-    enum LoadingState {
-        case loading, success, failed
-    }
-
-    @State private var loadingState = LoadingState.loading
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
 
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+                }
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6822), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+                }
+            }
         }
     }
 }
